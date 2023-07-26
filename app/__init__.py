b27cd0c858f3bdd7071835c3e6c2265502dd1812
Andrei <kr4806@yandex.ru>
2023-07-26 16:09:40 +0400

    Добавлена модель пользователя, добавлены миграции

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,16 @@
 from flask import Flask
 from .views import views
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from decouple import config
 from flask_debugtoolbar import DebugToolbarExtension
+import os
+
+db = SQLAlchemy()
+toolbar = DebugToolbarExtension()
+base_dir = os.path.dirname(__file__)
+MIGRATE_REPO = os.path.join(base_dir, 'migrations')
+migrate = Migrate()
 
 
 def create_app():
@@ -11,6 +19,8 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASES', cast=str)
     app.register_blueprint(views, url_prefix='/')
     app.debug = config('DEBUG', cast=bool)
-    db = SQLAlchemy(app)
-    toolbar = DebugToolbarExtension(app)
+    db.init_app(app)
+    toolbar.init_app(app)
+    migrate.init_app(app, db, MIGRATE_REPO)
+    from . import models
     return app
---
+++ app/models.py
@@ -0,0 +1,16 @@
+from . import db
+
+ROLE_USER = 0
+ROLE_ADMIN = 1
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), unique=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    role = db.Column(db.SmallInteger, default=ROLE_USER)
+
+    def __str__(self):
+        return f'User: {self.username}'
