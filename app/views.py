3d82ba591984952330188ab4c5eecca1af8fcbf5
SnoopyAizek <kr4806@yandex.ru>
2023-08-06 19:50:51 +0400

    Поправлен шаблон index, добавлены стили на будущее

--- app/views.py
+++ app/views.py
@@ -3,7 +3,7 @@ from flask import Blueprint, render_template, request
 views = Blueprint('views', __name__)
 
 
-@views.route("/")
+@views.route('/')
 def index():
     entered_name = request.args.get('entered_name', default=None, type=str)
     if entered_name and entered_name != '':
